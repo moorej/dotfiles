6d3fb0c18b2ec66cf8a3fae508f60c738f433cfc
Josh Moore <josh@moorej.net>
2024-02-21 21:34:43 -0600

    init

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- UI Config
+config.color_scheme = "Chalk (base16)"
+config.font_size = 13.0
+
+-- App Config
+config.quit_when_all_windows_are_closed = false
+
+return config

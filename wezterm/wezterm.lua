42ebe36b8c32c91931734dd28e15fab29ebff2c7
Josh Moore <mooreja557@me.com>
2024-02-22 09:15:35 -0600

    Wezterm fonts; Das F-keys

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- UI Config
-config.font = wezterm.font("FiraCode Nerd Font")
 config.color_scheme = "Chalk (base16)"
 config.font_size = 13.0
+
+config.font = wezterm.font_with_fallback({
+	{ family = "FiraCode Nerd Font", weight = "Medium" },
+})
 
 -- App Config
 config.quit_when_all_windows_are_closed = false

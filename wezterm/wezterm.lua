380da982b59ed88dfd2100f90f6b46c21046527f
Josh Moore <jomoore@hubspot.com>
2024-02-21 21:47:07 -0600

    fonts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- UI Config
+config.font = wezterm.font("FiraCode Nerd Font")
 config.color_scheme = "Chalk (base16)"
 config.font_size = 13.0
 

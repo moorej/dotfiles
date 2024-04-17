00a6ccbeeca745645153349048678dc97af48832
Josh Moore <mooreja557@me.com>
2024-04-17 17:59:42 -0500

    stuff

--- nvim/lua/moorej/remap_pre.lua
+++ nvim/lua/moorej/remap_pre.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Swap lines ala VSCode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- delete buffer without exiting
+vim.keymap.set("n", "<leader>q", ":bp<cr> :bd #<cr>", { desc = "Close the current buffer, switch to previous buffer" })
